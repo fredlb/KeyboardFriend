0b51daac635b0c1a37c398a9380772ca83c811ad
Fredrik Lindner <fredrik.lindner@gmail.com>
2023-07-06 21:36:12 +0200

    Initial Commit

---
+++ KeyboardFriend/KeyboardFriendApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeyboardFriendApp.swift
+//  KeyboardFriend
+//
+//  Created by Fredrik Lindner on 2023-07-06.
+//
+
+import SwiftUI
+
+@main
+struct KeyboardFriendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
